7bf12b8125be243dafcae680b5460697a0369234
eshf <62289445+eshf@users.noreply.github.com>
2023-07-11 17:51:09 +0800

    latest updates

--- frontend/app/guitest.js
+++ frontend/app/guitest.js
@@ -1,3 +1,10 @@
+<<<<<<< Updated upstream:frontend/app/guitest.js
 const title = document.querySelector("h1");
 const titleLength = title.textContent.length;
 title.style.setProperty("--typing-steps", titleLength);
+=======
+const title = document.querySelector("h1");
+const titleLength = title.textContent.length;
+title.style.setProperty("--typing-steps", titleLength);
+
+>>>>>>> Stashed changes:frontendapp/GUIPSI.js
---
+++ frontend/app/submitnpsi.js
@@ -0,0 +1,13 @@
+function submitFormData() {
+  let form = document.getElementById("npsiForm");
+  form.submit();
+}
+
+
+// Call C from JavaScript
+var result = Module.ccall('party3', // name of C function
+  'bigint', // return type
+  ['bigint'], // argument types
+  [myIdx], [setSize], [nTrials]); // arguments
+
+  Module.print()
