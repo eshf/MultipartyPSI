dbf5a7e3076c8a510ce792e4fd09c7c901861c61
eshf <62289445+eshf@users.noreply.github.com>
2023-07-13 10:16:50 +0800

    Revert "Merge pull request #10 from eshf/itsminori"
    This reverts commit 2b7166c8657ce857a4e6b13a7345d315b27b5fc9, reversing
    changes made to 8e1c19864b61100b8adb6a9b28995a340484ffea.

--- frontend/app/guitest.js
+++ frontend/app/guitest.js
@@ -1,10 +1,3 @@
-<<<<<<< Updated upstream:frontend/app/guitest.js
 const title = document.querySelector("h1");
 const titleLength = title.textContent.length;
 title.style.setProperty("--typing-steps", titleLength);
-=======
-const title = document.querySelector("h1");
-const titleLength = title.textContent.length;
-title.style.setProperty("--typing-steps", titleLength);
-
->>>>>>> Stashed changes:frontendapp/GUIPSI.js
--- frontend/submitnpsi.js
+++
@@ -1,4 +0,0 @@
-function submitFormData() {
-  let form = document.getElementById("npsiForm");
-  form.submit();
-}
