3bc1d2b90bee7ab48b5ed3659051c90ffdfb1549
eshf <62289445+eshf@users.noreply.github.com>
2023-08-28 15:16:35 +0800

    added npsi (docker version), merge branch

---
+++ npsi/CMakeFiles/frontend.exe.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/norieprojects/npsi/multipartypsi/frontend/CLP.cpp" "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/CLP.cpp.o" "gcc" "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/CLP.cpp.o.d"
+  "/home/norieprojects/npsi/multipartypsi/frontend/OtBinMain.cpp" "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/OtBinMain.cpp.o" "gcc" "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/OtBinMain.cpp.o.d"
+  "/home/norieprojects/npsi/multipartypsi/frontend/bitPosition.cpp" "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/bitPosition.cpp.o" "gcc" "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/bitPosition.cpp.o.d"
+  "/home/norieprojects/npsi/multipartypsi/frontend/main.cpp" "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/main.cpp.o" "gcc" "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/main.cpp.o.d"
+  "/home/norieprojects/npsi/multipartypsi/frontend/miraclTest.cpp" "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/miraclTest.cpp.o" "gcc" "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/miraclTest.cpp.o.d"
+  "/home/norieprojects/npsi/multipartypsi/frontend/util.cpp" "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/util.cpp.o" "gcc" "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/util.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ npsi/CMakeFiles/frontend.exe.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/CLP.cpp.o"
+  "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/CLP.cpp.o.d"
+  "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/OtBinMain.cpp.o"
+  "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/OtBinMain.cpp.o.d"
+  "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/bitPosition.cpp.o"
+  "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/bitPosition.cpp.o.d"
+  "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/main.cpp.o"
+  "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/main.cpp.o.d"
+  "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/miraclTest.cpp.o"
+  "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/miraclTest.cpp.o.d"
+  "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/util.cpp.o"
+  "CMakeFiles/frontend.exe.dir/multipartypsi/frontend/util.cpp.o.d"
+  "frontend.exe"
+  "frontend.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/frontend.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
